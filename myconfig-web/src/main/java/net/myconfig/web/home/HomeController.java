72ba9b6e492ef9405d8e547f5d818cb127dd82e9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-08 10:46:52 +0200

    Skeleton for the layout

--- myconfig-web/src/main/java/net/myconfig/web/home/HomeController.java
+++ myconfig-web/src/main/java/net/myconfig/web/home/HomeController.java
@@ -1,14 +1,15 @@
 package net.myconfig.web.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String home() {
-		// FIXME Loads the home content
+	public String home(Model model) {
+		// TODO Loads information about the REST APIs
 		// OK
 		return "home";
 	}
