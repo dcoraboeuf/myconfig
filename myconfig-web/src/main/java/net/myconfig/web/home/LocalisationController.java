799ebc1ea0dc5f83bd1844bf5a086d24dfe56c45
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-08 16:29:17 +0200

    Generation of localization file for the client side

---
+++ myconfig-web/src/main/java/net/myconfig/web/home/LocalisationController.java
@@ -0,0 +1,64 @@
+package net.myconfig.web.home;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jstring.Strings;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LocalisationController {
+
+    private final Strings strings;
+
+    @Autowired
+    public LocalisationController(Strings strings) {
+        this.strings = strings;
+    }
+
+    @RequestMapping(value = "/localization")
+    public void localisation(Locale locale, HttpServletResponse response) throws IOException {
+        // Locale
+        if (locale == null) {
+            locale = Locale.ENGLISH;
+        }
+        // Gets the list of key/values
+        Map<String, String> map = strings.getKeyValues(locale);
+        // Output
+        StringBuilder js = new StringBuilder();
+        js.append("// ").append(new Date()).append("\n");
+        js.append("var l = {\n");
+        int i = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (i > 0) {
+                js.append(",\n");
+            }
+            js.append(String.format("'%s': '%s'",
+                    key,
+                    StringEscapeUtils.escapeEcmaScript(value)
+            		));
+            i++;
+        }
+        js.append("\n};\n");
+        // Returns the response as JS
+        byte[] bytes = js.toString().getBytes("UTF-8");
+        response.setContentType("text/javascript");
+        response.setContentLength(bytes.length);
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(bytes);
+        outputStream.flush();
+        // TODO Cache management
+    }
+
+}
