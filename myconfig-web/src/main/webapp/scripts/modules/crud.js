c7a365924987b58be23393092e80bd5cbf769cf4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-11 20:42:55 +0200

    #6 Validation of the application name before creation

--- myconfig-web/src/main/webapp/scripts/modules/crud.js
+++ myconfig-web/src/main/webapp/scripts/modules/crud.js
@@ -4,6 +4,16 @@ var CRUD = function () {
 		confirmDelete: function (id) {
 			var value = document.getElementById ('crud-delete-prompt-' + id).value;
 			return confirm (value);
+		},
+		validateCreate: function (id) {
+			var htmlID = "#crud-create-text-" + id;
+			var value = $(htmlID).val();
+			if (value.trim() != "") {
+				return true;
+			} else {
+				$(htmlID).addClass("crud-invalid");
+				return false;
+			}
 		}
 	};
 	
