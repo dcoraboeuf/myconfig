c9d759f77c2c19868e8cb69e7ae8059dc188ba24
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-14 12:20:50 +0200

    #18 Switch the language using the location

--- myconfig-web/src/main/webapp/scripts/myconfig.js
+++ myconfig-web/src/main/webapp/scripts/myconfig.js
@@ -1,8 +1,14 @@
 var myconfig = function () {
 	
 	return {
-		changeLanguage: function (value) {
-			alert (value);
+		changeLanguage: function (lang) {
+			if (location.search.indexOf("language") > -1) {
+		      location.search = location.search.replace(/language=[a-z][a-z]/, "language=" + lang);
+		   } else if (location.search == "") {
+		      location.href += "?language=" + lang;
+		   } else {
+		      location.href += "&language=" + lang;
+		   }
 		}
 	};
 	
