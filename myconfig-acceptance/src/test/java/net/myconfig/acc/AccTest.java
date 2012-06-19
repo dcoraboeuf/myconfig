7255bf64ab1957f8e360b54569878d88d24e970c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-19 21:29:04 +0200

    #39 Setting the display for the firefox driver

--- myconfig-acceptance/src/test/java/net/myconfig/acc/AccTest.java
+++ myconfig-acceptance/src/test/java/net/myconfig/acc/AccTest.java
@@ -7,27 +7,33 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 public abstract class AccTest {
-	
+
 	private final SimpleDateFormat uidFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-	
+
 	protected WebDriver driver;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
+		FirefoxBinary firefox = new FirefoxBinary();
+		// FIXME Configuration of the display variable
+		firefox.setEnvironmentProperty("DISPLAY", ":" + 22);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		driver = new FirefoxDriver(firefox, firefoxProfile);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-    @After
-    public void tearDown() throws Exception {
-    	// Configuration: exiting the driver
-        driver.quit();
-    }
-	
-	protected String generateUniqueName (String prefix) {
+	@After
+	public void tearDown() throws Exception {
+		// Configuration: exiting the driver
+		driver.quit();
+	}
+
+	protected String generateUniqueName(String prefix) {
 		return prefix + generateUniqueId();
 	}
 
