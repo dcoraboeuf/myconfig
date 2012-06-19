cc3278fd66a5a7860196be5722e34d1b3867b08c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-19 21:36:52 +0200

    #39 Configurable display

--- myconfig-acceptance/src/test/java/net/myconfig/acc/AccTest.java
+++ myconfig-acceptance/src/test/java/net/myconfig/acc/AccTest.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -19,11 +20,16 @@ public abstract class AccTest {
 
 	@Before
 	public void setUp() throws Exception {
-		FirefoxBinary firefox = new FirefoxBinary();
-		// FIXME Configuration of the display variable
-		firefox.setEnvironmentProperty("DISPLAY", ":" + 22);
-		FirefoxProfile firefoxProfile = new FirefoxProfile();
-		driver = new FirefoxDriver(firefox, firefoxProfile);
+		String xvfbDisplay = System.getProperty("xvfb.display");
+		if (StringUtils.isNotBlank(xvfbDisplay)) {
+			System.out.println("Setting the Firefox driver on display " + xvfbDisplay);
+			FirefoxBinary firefox = new FirefoxBinary();
+			firefox.setEnvironmentProperty("DISPLAY", xvfbDisplay);
+			FirefoxProfile firefoxProfile = new FirefoxProfile();
+			driver = new FirefoxDriver(firefox, firefoxProfile);
+		} else {
+			driver = new FirefoxDriver();
+		}
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
