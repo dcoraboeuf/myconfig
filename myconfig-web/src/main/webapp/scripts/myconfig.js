659506676c3498eae0d6478d6e2cddfb2b58d334
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-21 14:41:39 +0200

    #15 Ajax call

--- myconfig-web/src/main/webapp/scripts/myconfig.js
+++ myconfig-web/src/main/webapp/scripts/myconfig.js
@@ -1,4 +1,34 @@
+String.prototype.format = function() {
+	var args = arguments;
+	return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function(m, n) {
+		if (m == "{{") {
+			return "{";
+		}
+		if (m == "}}") {
+			return "}";
+		}
+		return args[n];
+	});
+};
+
 var myconfig = function () {
+	
+	function displayAjaxError (message, jqXHR, textStatus, errorThrown) {
+		var text = '{0}\n[{1}] {2}'.format(message, jqXHR.status, jqXHR.statusText);
+		alert(text);
+	}
+	
+	function manageKeyVersion (application, version, key, set) {
+		var mode = set ? 'add' : 'remove';
+		var url = 'ui/version/' + application + '/' + version + '/' + mode + '/' + key;
+		$.post (url)
+			.success(function () {
+				alert('Success!');
+			})
+			.error(function (jqXHR, textStatus, errorThrown) {
+				displayAjaxError ('TODO Error', jqXHR, textStatus, errorThrown);
+			});
+	}
 	
 	return {
 		changeLanguage: function (lang) {
@@ -9,6 +39,12 @@ var myconfig = function () {
 		   } else {
 		      location.href += "&language=" + lang;
 		   }
+		},
+		addKeyVersion: function (application, version, key) {
+			manageKeyVersion (application, version, key, true);
+		},
+		removeKeyVersion: function (application, version, key) {
+			manageKeyVersion (application, version, key, false);
 		}
 	};
 	
