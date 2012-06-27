9a6fc3827e1a59f724a3edd6c0af78e4f71e4ab3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-06-27 14:01:56 +0200

    #16 Configuration page skeleton

---
+++ myconfig-web/src/main/java/net/myconfig/web/gui/GUIConfigurationPage.java
@@ -0,0 +1,31 @@
+package net.myconfig.web.gui;
+
+import net.myconfig.web.rest.UIInterface;
+import net.myconfig.web.support.ErrorHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/gui/configuration")
+public class GUIConfigurationPage extends AbstractGUIPage {
+
+	@Autowired
+	public GUIConfigurationPage(UIInterface ui, ErrorHandler errorHandler) {
+		super(ui, errorHandler);
+	}
+	
+	// FIXME Error management of the configuration page
+
+	@RequestMapping(value = "/{application:\\d+}/{version:.*}", method = RequestMethod.GET)
+	public String page(@PathVariable int application, @PathVariable String version, Model model) {
+		// FIXME Loads the configuration
+		// OK
+		return "configuration";
+	}
+
+}
