8f844755643644a0c57bbe1bd190dc91b14d267f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-07-24 21:03:51 +0200

    #61 IT database using a profile

--- myconfig-service-impl/src/main/java/net/myconfig/service/config/ProdConfiguration.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/config/ProdConfiguration.java
@@ -1,14 +1,8 @@
 package net.myconfig.service.config;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
 import net.myconfig.core.MyConfigProfiles;
 import net.myconfig.service.api.ConfigurationService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,25 +11,11 @@ import org.springframework.context.annotation.Profile;
 @Profile(MyConfigProfiles.PROD)
 public class ProdConfiguration extends CommonConfiguration {
 
-	private static final String JNDI_NAME = "jdbc/myconfig";
-	
-	private static final Logger log = LoggerFactory.getLogger(ProdConfiguration.class);
-
 	@Override
 	@Bean
 	public ConfigurationService configurationService() throws Exception {
 		// No logging file
 		return new DefaultConfigurationService(MyConfigProfiles.PROD, null);
-	}
-
-	@Override
-	@Bean
-	public DataSource dataSource() throws Exception {
-			log.info("[db] Using database at JNDI {}", JNDI_NAME);
-			// Creates the JNDI naming context
-			Context ctx = new InitialContext();
-			// Look-up
-			return (DataSource) ctx.lookup(JNDI_NAME);
 	}
 
 }
--- myconfig-service-impl/src/main/java/net/myconfig/service/config/UnitTestConfiguration.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/config/UnitTestConfiguration.java
@@ -1,13 +1,8 @@
 package net.myconfig.service.config;
 
-import javax.sql.DataSource;
-
 import net.myconfig.core.MyConfigProfiles;
 import net.myconfig.service.api.ConfigurationService;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,9 +10,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile(MyConfigProfiles.TEST)
 public class UnitTestConfiguration extends CommonConfiguration {
-
-	private static final Logger log = LoggerFactory
-			.getLogger(UnitTestConfiguration.class);
 	
 	@Override
 	@Bean
@@ -25,22 +17,6 @@ public class UnitTestConfiguration extends CommonConfiguration {
 		return new DefaultConfigurationService(
 				MyConfigProfiles.TEST,
 				"classpath:log4j_dev.properties");
-	}
-
-	@Override
-	@Bean
-	public DataSource dataSource() {
-		String dbURL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-		log.info("Using database at {}", dbURL);
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName("org.h2.Driver");
-		ds.setUrl(dbURL);
-		ds.setUsername("sa");
-		ds.setPassword("");
-		ds.setDefaultAutoCommit(false);
-		ds.setInitialSize(1);
-		ds.setMaxActive(2);
-		return ds;
 	}
 
 }
