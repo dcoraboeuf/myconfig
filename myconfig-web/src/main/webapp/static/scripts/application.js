238ad9612448c313dbdc50dff63db389d36e61e4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-07-27 10:31:40 +0200

    #20 Auto-size of the dialog

--- myconfig-web/src/main/webapp/static/scripts/application.js
+++ myconfig-web/src/main/webapp/static/scripts/application.js
@@ -3,11 +3,11 @@ var Application = function () {
 	function editKeyDescription (key) {
 		var id = 'key_' + key;
 		var value = document.getElementById(id).getAttribute('description');
-		document.getElementById('dialog-key-name').innerHTML = key;
-		document.getElementById('dialog-key-name-value').value = key;
+		document.getElementById('dialog-key-name').value = key;
 		document.getElementById('dialog-key-description').value = value;
 		$('#dialog-key-update').dialog({
-			width: '30%'
+			title: loc('application.key.update.title', key),
+			width: 'auto'
 		});
 	}
 
