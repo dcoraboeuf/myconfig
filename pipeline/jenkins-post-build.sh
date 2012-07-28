0929b06caa081a2767aae5347576a1eff16aa306
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-07-28 13:56:40 +0200

    #66 Tagging after build

---
+++ pipeline/jenkins-post-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script must be executed after the build is complete and successful
+
+# We don't care about Jenkins tags
+for TAG in $(git tag -l jenkins-myconfig*) ; do git tag -d $TAG; done
+# Tag
+TAG=b${BUILD_NUMBER}
+# Tagging the build
+git tag ${TAG}
+# Pushing the result
+git push origin ${TAG}
+
