9c8970c6244321e5f2867fb11dc02471f5fc52c0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-07-31 19:55:50 +0200

    #66 Deployment and error handling

--- pipeline/jenkins-build.sh
+++ pipeline/jenkins-build.sh
@@ -4,6 +4,28 @@
 # BUILD_NUMBER environment variable must be accessible
 # mvn must be accessible and correctly configured
 
+#############################
+# Check environment variables
+#############################
+
+if [ "$BUILD_NUMBER" == "" ]
+then
+	echo BUILD_NUMBER is required.
+	exit 1
+fi
+
+if [ "$NEXUS_URL" == "" ]
+then
+        echo NEXUS_URL is required.
+        exit 1
+fi
+
+if [ "$NEXUS_ID" == "" ]
+then
+        echo NEXUS_ID is required.
+        exit 1
+fi
+
 ##########################
 # Preparation of the build
 ##########################
@@ -14,27 +36,50 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[0-9]" | sed -e
 # Release number is made of the version and the build number
 RELEASE=${VERSION}-${BUILD_NUMBER}
 
-# Logging the whole
-echo Building release ${RELEASE}...
-
 #####################################################
 # Runs the build itself and as much tests as possible
 #####################################################
 
 # Build options
+echo Building release ${RELEASE}...
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128m"
 
 # Maven build
 mvn clean install -P acceptance-test
+if [ $? -ne 0 ]
+then
+	echo Build failed.
+	exit 1
+fi
 
 # Creates the TAR that contains:
 # 1. the WAR
 # 2. all the deployment files
-tar --create --gzip --file=target/myconfig-${RELEASE}.tar --directory=myconfig-web/target myconfig-web-${RELEASE}.war
+echo Creating the archive...
+tar --create --gzip --file=target/myconfig-${RELEASE}.tgz --directory=myconfig-web/target myconfig-web-${RELEASE}.war
+if [ $? -ne 0]
+then
+	echo Archiving failed.
+	exit 1
+fi
+
+###############################
+# Upload the archive into Nexus
+###############################
+
+echo Uploading to Nexus @ ${NEXUS_URL} (id = ${NEXUS_ID})
+mvn deploy:deploy-file -Dfile=target/myconfig-${RELEASE}.tgz -DrepositoryId=${NEXUS_ID} -Durl=${NEXUS_URL} -DgroupId=net.myconfig -DartifactId=myconfig -Dversion=${RELEASE} -DgeneratePom=true -Dpackaging=tgz
+if [ $? -ne 0]
+then
+	echo Deployment failed.
+	exit 1
+fi
 
 ##################################################################
 # After the build is complete, create the tag in Git and pushes it
 ##################################################################
+
+echo Tagging...
 
 # We don't care about Jenkins tags
 for TAG in $(git tag -l jenkins-myconfig*) ; do
@@ -47,3 +92,6 @@ TAG=myconfig-${RELEASE}
 git tag ${TAG}
 # Pushing the result
 git push origin ${TAG}
+
+echo Done.
+
