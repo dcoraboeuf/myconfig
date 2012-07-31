6c9c02c1b759cbaccab65dbeef8aeb87e764a705
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-07-31 10:17:57 +0200

    #66 Build file

---
+++ pipeline/jenkins-build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Pre-requisites:
+# BUILD_NUMBER environment variable must be accessible
+# mvn must be accessible and correctly configured
+
+##########################
+# Preparation of the build
+##########################
+
+# Gets the version number from the POM
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[0-9]" | sed -e 's/\-SNAPSHOT//'`
+
+# Release number is made of the version and the build number
+RELEASE=${VERSION}-${BUILD_NUMBER}
+
+# Logging the whole
+echo Building release ${RELEASE}...
+
+#####################################################
+# Runs the build itself and as much tests as possible
+#####################################################
+
+# Maven build
+mvn clean install -P acceptance-test
+
+# Creates the TAR that contains:
+# 1. the WAR
+# 2. all the deployment files
+tar --create --gzip --file=target/myconfig-${RELEASE}.tar --directory=myconfig-web/target myconfig-web-${RELEASE}.war
+
+##################################################################
+# After the build is complete, create the tag in Git and pushes it
+##################################################################
+
+# We don't care about Jenkins tags
+for TAG in $(git tag -l jenkins-myconfig*) ; do
+	git tag -d $TAG
+done
+
+# Tag
+TAG=myconfig-${RELEASE}
+# Tagging the build
+git tag ${TAG}
+# Pushing the result
+git push origin ${TAG}
--- pipeline/jenkins-pre-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[0-9]" | sed -e 's/\-SNAPSHOT//'`
-RELEASE=${VERSION}-${BUILD_NUMBER}
-echo Release ${RELEASE}
-echo VERSION=${VERSION} > build.properties
-echo RELEASE=${RELEASE} >> build.properties
-
