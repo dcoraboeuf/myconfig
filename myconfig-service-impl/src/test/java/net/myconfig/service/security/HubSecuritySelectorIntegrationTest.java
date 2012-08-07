88a2c7861f58096e000979cb3dcabeec73accfae
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-08-07 15:26:48 +0200

    #80 Correction of unit tests

--- myconfig-service-impl/src/test/java/net/myconfig/service/security/HubSecuritySelectorIntegrationTest.java
+++ myconfig-service-impl/src/test/java/net/myconfig/service/security/HubSecuritySelectorIntegrationTest.java
@@ -3,12 +3,17 @@ package net.myconfig.service.security;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import net.myconfig.service.api.security.SecuritySelector;
 import net.myconfig.test.AbstractIntegrationTest;
+import net.myconfig.test.DBUnitHelper;
 
+import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.DataSetException;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,6 +21,22 @@ public class HubSecuritySelectorIntegrationTest extends AbstractIntegrationTest 
 
 	@Autowired
 	private SecuritySelector selector;
+
+	@Before
+	public void before() throws SQLException {
+		IDatabaseConnection databaseConnection = DBUnitHelper.getConnection();
+		Connection c = databaseConnection.getConnection();
+		try {
+			PreparedStatement ps = c.prepareStatement("delete from configuration where name = 'security.mode'");
+			try {
+				ps.executeUpdate();
+			} finally {
+				ps.close();
+			}
+		} finally {
+			c.commit();
+		}
+	}
 
 	@Test
 	public void getSecurityManagementId() {
