8562f27180722d9c99a611325b9615b4b44d756b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-08-07 15:43:10 +0200

    #81 Security unit tests

--- myconfig-service-impl/src/test/java/net/myconfig/service/impl/SecurityServiceSecurityTest.java
+++ myconfig-service-impl/src/test/java/net/myconfig/service/impl/SecurityServiceSecurityTest.java
@@ -1,19 +1,30 @@
 package net.myconfig.service.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import net.myconfig.core.MyConfigRoles;
+import net.myconfig.core.UserFunction;
 import net.myconfig.service.api.security.SecurityService;
 import net.myconfig.service.api.security.UserToken;
+import net.myconfig.service.model.Ack;
 import net.myconfig.service.model.UserSummary;
 import net.myconfig.service.security.UserAuthenticationToken;
 import net.myconfig.test.AbstractIntegrationTest;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -36,6 +47,10 @@ public class SecurityServiceSecurityTest extends AbstractIntegrationTest {
 
 	@Before
 	public void cleanContext() {
+		// Sets the security mode
+		asAdmin();
+		service.setSecurityMode("builtin");
+		// No context
 		SecurityContextImpl context = new SecurityContextImpl();
 		context.setAuthentication(new AnonymousAuthenticationToken("anonymous", "anonymous", AuthorityUtils.createAuthorityList(MyConfigRoles.ANONYMOUS)));
 		SecurityContextHolder.setContext(context);
@@ -59,6 +74,141 @@ public class SecurityServiceSecurityTest extends AbstractIntegrationTest {
 		assertNotNull(users);
 	}
 
+	@Test
+	public void getUserList_user_granted() {
+		asUser(UserFunction.security_users);
+		List<UserSummary> users = service.getUserList();
+		assertNotNull(users);
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void getUserList_user_not_granted() {
+		asUser();
+		service.getUserList();
+	}
+
+	@Test
+	public void userCreate_admin() {
+		asAdmin();
+		Ack ack = service.userCreate("test");
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test
+	public void userCreate_user_granted() {
+		asUser(UserFunction.security_users);
+		Ack ack = service.userCreate("test");
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void userCreate_user_not_granted() {
+		asUser();
+		service.userCreate("test");
+	}
+
+	@Test
+	public void userDelete_admin() {
+		asAdmin();
+		Ack ack = service.userDelete("test");
+		assertFalse(ack.isSuccess());
+	}
+
+	@Test
+	public void userDelete_user_granted() {
+		asUser(UserFunction.security_users);
+		Ack ack = service.userDelete("test");
+		assertFalse(ack.isSuccess());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void userDelete_user_not_granted() {
+		asUser();
+		service.userDelete("test");
+	}
+
+	@Test
+	public void userFunctionAdd_admin() {
+		asAdmin();
+		Ack ack = service.userFunctionAdd("user1", UserFunction.app_create);
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test
+	public void userFunctionAdd_user_granted() {
+		asUser(UserFunction.security_users);
+		Ack ack = service.userFunctionAdd("user1", UserFunction.app_create);
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void userFunctionAdd_user_not_granted() {
+		asUser();
+		service.userFunctionAdd("user1", UserFunction.app_create);
+	}
+
+	@Test
+	public void userFunctionRemove_admin() {
+		asAdmin();
+		Ack ack = service.userFunctionRemove("user1", UserFunction.app_create);
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test
+	public void userFunctionRemove_user_granted() {
+		asUser(UserFunction.security_users);
+		Ack ack = service.userFunctionRemove("user1", UserFunction.app_create);
+		assertTrue(ack.isSuccess());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void userFunctionRemove_user_not_granted() {
+		asUser();
+		service.userFunctionRemove("user1", UserFunction.app_create);
+	}
+
+	@Test
+	public void getSecurityMode_no_control() {
+		assertEquals("builtin", service.getSecurityMode());
+	}
+
+	@Test
+	public void setSecurityMode_admin() {
+		asAdmin();
+		service.setSecurityMode("xxx");
+		assertEquals("xxx", service.getSecurityMode());
+	}
+
+	@Test
+	public void setSecurityMode_user_granted() {
+		asUser(UserFunction.security_setup);
+		service.setSecurityMode("xxx");
+		assertEquals("xxx", service.getSecurityMode());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void setSecurityMode_user_not_granted() {
+		asUser();
+		service.setSecurityMode("xxx");
+	}
+
+	protected void asUser(final UserFunction... functions) {
+		SecurityContextImpl context = new SecurityContextImpl();
+
+		UserToken token = mock(UserToken.class);
+		when(token.hasUserFunction(any(UserFunction.class))).thenAnswer(new Answer<Boolean>() {
+			@Override
+			public Boolean answer(InvocationOnMock invocation) throws Throwable {
+				UserFunction fn = (UserFunction) invocation.getArguments()[0];
+				return ArrayUtils.contains(functions, fn);
+			}
+		});
+
+		Authentication authentication = Mockito.mock(Authentication.class);
+		context.setAuthentication(new UserAuthenticationToken(token, authentication));
+		SecurityContextHolder.setContext(context);
+	}
+
 	protected void asAdmin() {
 		SecurityContextImpl context = new SecurityContextImpl();
 		UserToken token = service.getUserToken("admin", "admin");
@@ -66,17 +216,4 @@ public class SecurityServiceSecurityTest extends AbstractIntegrationTest {
 		context.setAuthentication(new UserAuthenticationToken(token, authentication));
 		SecurityContextHolder.setContext(context);
 	}
-
-	//
-	// Ack userCreate(String name);
-	//
-	// Ack userDelete(String name);
-	//
-	// Ack userFunctionAdd(String name, UserFunction fn);
-	//
-	// Ack userFunctionRemove(String name, UserFunction fn);
-	//
-	// String getSecurityMode();
-	//
-	// void setSecurityMode(String mode);
 }
