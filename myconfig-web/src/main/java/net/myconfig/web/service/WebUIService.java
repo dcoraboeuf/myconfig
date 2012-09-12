5f8b16539aa1896d3dea0fa6679f0c7f5db93780
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-09-12 21:35:42 +0200

    #93 Added some debug information about the request used when creating the template

--- myconfig-web/src/main/java/net/myconfig/web/service/WebUIService.java
+++ myconfig-web/src/main/java/net/myconfig/web/service/WebUIService.java
@@ -1,15 +1,28 @@
 package net.myconfig.web.service;
 
 import static java.lang.String.format;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import net.myconfig.service.api.UIService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebUIService implements UIService {
+	
+	private final Logger logger = LoggerFactory.getLogger(UIService.class);
 
 	@Override
 	public String getLink(Link link, String... components) {
+		// Gets the request from the content
+		HttpServletRequest request = UIInterceptor.getCurrentRequest();
+		// Logs everything about the request
+		log(request);
 		// FIXME Gets the root URL
 		String root = "http://localhost:8080/myconfig";
 		// Building the variable part
@@ -26,6 +39,27 @@ public class WebUIService implements UIService {
 		}
 		// OK
 		return format("%s/%s", root, query);
+	}
+
+	private void log(HttpServletRequest request) {
+		logger.debug("[request] Context path: {}", request.getContextPath());
+		logger.debug("[request] Server name: {}", request.getServerName());
+		logger.debug("[request] Server port: {}", request.getServerPort());
+		logger.debug("[request] Request URL: {}", request.getRequestURL());
+		logger.debug("[request] Protocol: {}", request.getProtocol());
+		logger.debug("[request] Scheme: {}", request.getScheme());
+		logger.debug("[request] Servlet path: {}", request.getServletPath());
+		@SuppressWarnings("unchecked")
+		Enumeration<String> names = request.getHeaderNames();
+		while (names.hasMoreElements()) {
+			String name = names.nextElement();
+			@SuppressWarnings("unchecked")
+			Enumeration<String> values = request.getHeaders(name);
+			while (values.hasMoreElements()) {
+				String value = values.nextElement();
+				logger.debug("[request] Header {} = {}", name, value);
+			}
+		}
 	}
 
 }
