863ac21e5eb7844e9b818738dc75a4a41a9543c0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-09-13 20:25:25 +0200

    #95 Initial values for the configuration

--- myconfig-service-api/src/main/java/net/myconfig/service/api/ConfigurationService.java
+++ myconfig-service-api/src/main/java/net/myconfig/service/api/ConfigurationService.java
@@ -5,6 +5,15 @@ public interface ConfigurationService {
 	String SECURITY_MODE = "security.mode";
 	String SECURITY_MODE_DEFAULT = "none";
 
+	String APP_NAME = "app.name";
+	String APP_NAME_DEFAULT = "myconfig";
+	
+	String APP_REPLYTO_ADDRESS = "app.replyto.address";
+	String APP_REPLYTO_ADDRESS_DEFAULT = "noreply@myconfig.net";
+
+	String APP_REPLYTO_NAME = "app.replyto.name";
+	String APP_REPLYTO_NAME_DEFAULT = "the myconfig team";
+
 	String getParameter(String name, String defaultValue);
 
 	void setParameter(String name, String value);
--- myconfig-service-impl/src/main/java/net/myconfig/service/message/MailPost.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/message/MailPost.java
@@ -4,6 +4,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import net.myconfig.core.MyConfigProfiles;
+import net.myconfig.service.api.ConfigurationService;
 import net.myconfig.service.api.message.Message;
 import net.myconfig.service.api.message.MessageChannel;
 import net.myconfig.service.model.Ack;
@@ -25,10 +26,12 @@ public class MailPost extends AbstractMessagePost {
 	private final Logger logger = LoggerFactory.getLogger(MailPost.class);
 
 	private final JavaMailSender mailSender;
+	private final ConfigurationService configurationService;
 
 	@Autowired
-	public MailPost(JavaMailSender mailSender) {
+	public MailPost(JavaMailSender mailSender, ConfigurationService configurationService) {
 		this.mailSender = mailSender;
+		this.configurationService = configurationService;
 	}
 
 	@Override
@@ -38,14 +41,15 @@ public class MailPost extends AbstractMessagePost {
 
 	@Override
 	public Ack post(final Message message, final String destination) {
+		
+		final String replyToAddress = configurationService.getParameter(ConfigurationService.APP_REPLYTO_ADDRESS, ConfigurationService.APP_REPLYTO_ADDRESS_DEFAULT);
 
 		MimeMessagePreparator preparator = new MimeMessagePreparator() {
 
-			// FIXME Configuration: from email address
 			@Override
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				mimeMessage.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(destination));
-				mimeMessage.setFrom(new InternetAddress("mail@mycompany.com"));
+				mimeMessage.setFrom(new InternetAddress(replyToAddress));
 				mimeMessage.setSubject(message.getTitle());
 				mimeMessage.setText(message.getContent());
 			}
