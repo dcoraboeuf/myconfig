c4c4980a8c9bf10c728ea873113b32d4225e2c01
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-09-22 19:52:51 +0200

    #104 Using Lombok

--- myconfig-service-api/src/main/java/net/myconfig/service/model/Key.java
+++ myconfig-service-api/src/main/java/net/myconfig/service/model/Key.java
@@ -1,26 +1,11 @@
 package net.myconfig.service.model;
 
+import lombok.Data;
+
+@Data
 public class Key {
 
 	private final String name;
 	private final String description;
-
-	public Key(String name, String description) {
-		this.name = name;
-		this.description = description;
-	}
-
-	public String getName() {
-		return name;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-
-	@Override
-	public String toString() {
-		return name;
-	}
 
 }
