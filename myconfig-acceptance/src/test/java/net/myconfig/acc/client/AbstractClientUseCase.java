5be14ccc3285694aceddb53f7a37a4d1eb11c9cd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-10-12 16:16:57 +0200

    #36 Application integration test

--- myconfig-acceptance/src/test/java/net/myconfig/acc/client/AbstractClientUseCase.java
+++ myconfig-acceptance/src/test/java/net/myconfig/acc/client/AbstractClientUseCase.java
@@ -1,14 +1,23 @@
 package net.myconfig.acc.client;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import net.myconfig.client.java.MyConfigClient;
 import net.myconfig.client.java.support.MyConfigClientFactory;
+import net.myconfig.core.model.Ack;
+import net.myconfig.core.model.ApplicationSummaries;
+import net.myconfig.core.model.ApplicationSummary;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.BeforeClass;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 public abstract class AbstractClientUseCase {
 
@@ -35,5 +44,26 @@ public abstract class AbstractClientUseCase {
 		String url = String.format("http://localhost:%s/myconfig/ui/", itPort);
 		// Creates the client for the API
 		client = MyConfigClientFactory.create(url).build();
+	}
+
+	protected int application_create(String appName) {
+		ApplicationSummary summary = client.applicationCreate(appName);
+		assertNotNull(summary);
+		return summary.getId();
+	}
+
+	protected void application_delete(int id) {
+		Ack ack = client.applicationDelete(id);
+		assertTrue("Cannot delete application " + id, ack.isSuccess());
+	}
+
+	protected ApplicationSummary application_summary(final int id) {
+		ApplicationSummaries applications = client.applications();
+		return Iterables.find(applications.getSummaries(), new Predicate<ApplicationSummary>() {
+			@Override
+			public boolean apply(ApplicationSummary o) {
+				return id == o.getId();
+			}
+		});
 	}
 }
--- myconfig-acceptance/src/test/java/net/myconfig/acc/client/ITRESTUI.java
+++ myconfig-acceptance/src/test/java/net/myconfig/acc/client/ITRESTUI.java
@@ -1,6 +1,11 @@
 package net.myconfig.acc.client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.UUID;
+
+import net.myconfig.core.model.ApplicationSummary;
 
 import org.junit.Test;
 
@@ -10,6 +15,24 @@ public class ITRESTUI extends AbstractClientUseCase {
 	public void version() {
 		String actualVersion = client.version();
 		assertEquals(version, actualVersion);
+	}
+	
+	@Test
+	public void application() {
+		// Creates a unique application
+		String uuid = UUID.randomUUID().toString();
+		String appName = "test_" + uuid;
+		int id = application_create (appName);
+		// Gets the application summary
+		ApplicationSummary summary = application_summary (id);
+		assertEquals (id, summary.getId());
+		assertEquals (appName, summary.getName());
+		// TODO Basic stats
+		// Deletes the application		
+		application_delete (id);
+		// Checks it has been deleted
+		summary = application_summary(id);
+		assertNull (summary);
 	}
 
 }
