c2afc6b7e36372fe9f3aac906075310c0a509983
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-10-17 12:41:37 +0200

    [lombok-json] Using the legacy annotation introspector as well

--- myconfig-core/src/main/java/com/netbeetle/jackson/ObjectMapperFactory.java
+++ myconfig-core/src/main/java/com/netbeetle/jackson/ObjectMapperFactory.java
@@ -1,16 +1,17 @@
 package com.netbeetle.jackson;
 
+import org.codehaus.jackson.map.AnnotationIntrospector;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
-
 
 public final class ObjectMapperFactory {
 
 	public static ObjectMapper createObjectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
-		DeserializationConfig config = mapper.getDeserializationConfig().withAnnotationIntrospector(new ConstructorPropertiesAnnotationIntrospector());
-		mapper.setDeserializationConfig(config);
-		return mapper;
+		DeserializationConfig config = mapper.getDeserializationConfig();
+		AnnotationIntrospector pair = new AnnotationIntrospector.Pair(config.getAnnotationIntrospector(), new ConstructorPropertiesAnnotationIntrospector());
+		config = config.withAnnotationIntrospector(pair);
+		return mapper.setDeserializationConfig(config);
 	}
 
 	private ObjectMapperFactory() {
