8ba34fb1ea8fe852097b7f0992bcedd1954cee1b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-10-17 20:34:45 +0200

    [lombok-json] JSON ser/deser tests

--- myconfig-core/src/test/groovy/net/myconfig/core/model/JsonConfigurationUpdatesTest.groovy
+++ myconfig-core/src/test/groovy/net/myconfig/core/model/JsonConfigurationUpdatesTest.groovy
@@ -7,7 +7,7 @@ import com.netbeetle.jackson.ObjectMapperFactory;
 class JsonConfigurationUpdatesTest {
 	
 	@Test
-	void json() {
+	void ser_deser() {
 		def mapper = ObjectMapperFactory.createObjectMapper()
 		// Creates the configuration
 		List<ConfigurationUpdate> updates = new ArrayList<ConfigurationUpdate>()
@@ -25,6 +25,30 @@ class JsonConfigurationUpdatesTest {
 		def configurationUpdates = new ConfigurationUpdates(updates)
 		// Converts to JSON
 		def json = mapper.writeValueAsString(configurationUpdates)
+		// Reads from JSON
+		def o = mapper.readValue(json, ConfigurationUpdates.class)
+		// Test
+		assert o == configurationUpdates
+	}
+	
+	@Test
+	void deser() {
+		def json = """{"updates":[{"environment":"DEV","version":"1.0","key":"jdbc.user","value":"1.0 DEV jdbc.user"},{"environment":"ACC","version":"1.0","key":"jdbc.user","value":"1.0 ACC jdbc.user"},{"environment":"UAT","version":"1.0","key":"jdbc.user","value":"1.0 UAT jdbc.user"},{"environment":"PROD","version":"1.0","key":"jdbc.user","value":"1.0 PROD jdbc.user"},{"environment":"DEV","version":"1.0","key":"jdbc.password","value":"1.0 DEV jdbc.password"},{"environment":"ACC","version":"1.0","key":"jdbc.password","value":"1.0 ACC jdbc.password"},{"environment":"UAT","version":"1.0","key":"jdbc.password","value":"1.0 UAT jdbc.password"},{"environment":"PROD","version":"1.0","key":"jdbc.password","value":"1.0 PROD jdbc.password"},{"environment":"DEV","version":"1.1","key":"jdbc.user","value":"1.1 DEV jdbc.user"},{"environment":"ACC","version":"1.1","key":"jdbc.user","value":"1.1 ACC jdbc.user"},{"environment":"UAT","version":"1.1","key":"jdbc.user","value":"1.1 UAT jdbc.user"},{"environment":"PROD","version":"1.1","key":"jdbc.user","value":"1.1 PROD jdbc.user"},{"environment":"DEV","version":"1.1","key":"jdbc.password","value":"1.1 DEV jdbc.password"},{"environment":"ACC","version":"1.1","key":"jdbc.password","value":"1.1 ACC jdbc.password"},{"environment":"UAT","version":"1.1","key":"jdbc.password","value":"1.1 UAT jdbc.password"},{"environment":"PROD","version":"1.1","key":"jdbc.password","value":"1.1 PROD jdbc.password"},{"environment":"DEV","version":"1.2","key":"jdbc.user","value":"1.2 DEV jdbc.user"},{"environment":"ACC","version":"1.2","key":"jdbc.user","value":"1.2 ACC jdbc.user"},{"environment":"UAT","version":"1.2","key":"jdbc.user","value":"1.2 UAT jdbc.user"},{"environment":"PROD","version":"1.2","key":"jdbc.user","value":"1.2 PROD jdbc.user"},{"environment":"DEV","version":"1.2","key":"jdbc.password","value":"1.2 DEV jdbc.password"},{"environment":"ACC","version":"1.2","key":"jdbc.password","value":"1.2 ACC jdbc.password"},{"environment":"UAT","version":"1.2","key":"jdbc.password","value":"1.2 UAT jdbc.password"},{"environment":"PROD","version":"1.2","key":"jdbc.password","value":"1.2 PROD jdbc.password"}]}"""
+		def mapper = ObjectMapperFactory.createObjectMapper()
+		// Creates the expected configuration
+		List<ConfigurationUpdate> updates = new ArrayList<ConfigurationUpdate>()
+		["1.0", "1.1", "1.2"].each {
+			version ->
+				["jdbc.user", "jdbc.password"].each {
+					key ->
+						["DEV", "ACC", "UAT", "PROD"].each {
+							env -> 
+								def value = "$version $env $key"
+								updates.add(new ConfigurationUpdate(env, version, key, value))
+						}
+				}
+		}
+		def configurationUpdates = new ConfigurationUpdates(updates)
 		// Reads from JSON
 		def o = mapper.readValue(json, ConfigurationUpdates.class)
 		// Test
