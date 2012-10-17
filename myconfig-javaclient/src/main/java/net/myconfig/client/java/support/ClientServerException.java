05b5c6b156686ea3fef03230231da7bba6b46373
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-10-17 20:22:09 +0200

    #36 Default message for ClientServerException

--- myconfig-javaclient/src/main/java/net/myconfig/client/java/support/ClientServerException.java
+++ myconfig-javaclient/src/main/java/net/myconfig/client/java/support/ClientServerException.java
@@ -1,11 +1,31 @@
 package net.myconfig.client.java.support;
 
+import java.util.Locale;
+
 import net.myconfig.core.CoreException;
 
 public class ClientServerException extends CoreException {
+	
+	private final String message;
 
 	public ClientServerException(Object request, int statusCode, String reasonPhrase) {
 		super(request, statusCode, reasonPhrase);
+		this.message = String.format("%s [%d] %s", request, statusCode, reasonPhrase);
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
+	}
+	
+	@Override
+	public String getLocalizedMessage() {
+		return message;
+	}
+	
+	@Override
+	public String getLocalizedMessage(Locale locale) {
+		return message;
 	}
 
 }
