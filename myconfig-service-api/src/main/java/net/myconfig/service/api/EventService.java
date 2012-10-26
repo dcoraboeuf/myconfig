4162ccdcd94bd3137880f6b1df24520868523dcb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-10-26 20:27:09 +0200

    #21 Event service API

---
+++ myconfig-service-api/src/main/java/net/myconfig/service/api/EventService.java
@@ -0,0 +1,11 @@
+package net.myconfig.service.api;
+
+import net.myconfig.core.model.Event;
+
+public interface EventService {
+	
+	void saveEvent (Event event);
+	
+	// TODO Gets a list of events according to some criterias
+
+}
