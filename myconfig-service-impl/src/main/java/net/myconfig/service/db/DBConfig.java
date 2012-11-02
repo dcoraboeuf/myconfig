67f57d26fb9b1b61ae8e45dad50e2823c2d968a0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-02 16:54:39 +0100

    #120 Change the database schema

--- myconfig-service-impl/src/main/java/net/myconfig/service/db/DBConfig.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/db/DBConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 9;
+	public static final int VERSION = 10;
 	
 	@Autowired
 	private DataSource dataSource;
