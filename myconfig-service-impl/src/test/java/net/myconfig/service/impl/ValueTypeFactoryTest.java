4b1b387b9ce5f646fae135e59037fc30446c89c1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-07 13:25:54 +0100

    #119 Getting the list of types: unit test

--- myconfig-service-impl/src/test/java/net/myconfig/service/impl/ValueTypeFactoryTest.java
+++ myconfig-service-impl/src/test/java/net/myconfig/service/impl/ValueTypeFactoryTest.java
@@ -2,7 +2,13 @@ package net.myconfig.service.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import net.myconfig.core.type.ValueType;
+import net.myconfig.core.type.ValueTypeDescription;
+import net.myconfig.core.type.ValueTypeDescriptions;
 import net.myconfig.service.api.type.ValueTypeFactory;
 import net.myconfig.service.api.type.ValueTypeNotFoundException;
 import net.myconfig.test.AbstractIntegrationTest;
@@ -10,10 +16,28 @@ import net.myconfig.test.AbstractIntegrationTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
 public class ValueTypeFactoryTest extends AbstractIntegrationTest {
 
 	@Autowired
 	private ValueTypeFactory factory;
+
+	@Test
+	public void types() {
+		ValueTypeDescriptions list = factory.getValueTypeDescriptions();
+		assertNotNull(list);
+		List<ValueTypeDescription> descriptions = list.getDescriptions();
+		assertNotNull(descriptions);
+		assertTrue(descriptions.size() > 1);
+		assertNotNull(Iterables.find(descriptions, new Predicate<ValueTypeDescription>() {
+			@Override
+			public boolean apply(ValueTypeDescription o) {
+				return "plain".equals(o.getId());
+			}
+		}));
+	}
 
 	@Test(expected = ValueTypeNotFoundException.class)
 	public void typeNotFound() {
