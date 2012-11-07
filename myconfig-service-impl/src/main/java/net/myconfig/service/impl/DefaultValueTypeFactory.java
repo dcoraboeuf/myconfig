3fe3f830183c94480702ba578f98f7746fd58f4f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-07 14:41:13 +0100

    #119 The 'plain' type must be the first one in the list

--- myconfig-service-impl/src/main/java/net/myconfig/service/impl/DefaultValueTypeFactory.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/impl/DefaultValueTypeFactory.java
@@ -1,6 +1,8 @@
 package net.myconfig.service.impl;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import net.myconfig.core.type.ValueType;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 
 @Service
@@ -24,12 +27,26 @@ public class DefaultValueTypeFactory implements ValueTypeFactory {
 
 	@Autowired
 	public DefaultValueTypeFactory(Collection<ValueType> types) {
-		index = Maps.uniqueIndex(types, new Function<ValueType, String>() {
+		Map<String, ValueType> initialIndex = new HashMap<String, ValueType>(Maps.uniqueIndex(types, new Function<ValueType, String>() {
 			@Override
 			public String apply(ValueType type) {
 				return type.getId();
 			}
-		});
+		}));
+		// Plain type
+		ValueType plainType = initialIndex.get(ValueType.PLAIN);
+		if (plainType == null) {
+			throw new IllegalStateException("'plain' value type must be defined");
+		}
+		initialIndex.remove(ValueType.PLAIN);
+		// Result
+		Map<String, ValueType> result = new LinkedHashMap<String, ValueType>();
+		// Makes sure 'plain' is first
+		result.put(plainType.getId(), plainType);
+		// Adds the result
+		result.putAll(initialIndex);
+		// OK
+		index = ImmutableMap.copyOf(result);
 	}
 
 	@Override
--- myconfig-service-impl/src/test/java/net/myconfig/service/impl/ValueTypeFactoryTest.java
+++ myconfig-service-impl/src/test/java/net/myconfig/service/impl/ValueTypeFactoryTest.java
@@ -1,6 +1,7 @@
 package net.myconfig.service.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -16,9 +17,6 @@ import net.myconfig.test.AbstractIntegrationTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-
 public class ValueTypeFactoryTest extends AbstractIntegrationTest {
 
 	@Autowired
@@ -31,12 +29,9 @@ public class ValueTypeFactoryTest extends AbstractIntegrationTest {
 		List<ValueTypeDescription> descriptions = list.getDescriptions();
 		assertNotNull(descriptions);
 		assertTrue(descriptions.size() > 1);
-		assertNotNull(Iterables.find(descriptions, new Predicate<ValueTypeDescription>() {
-			@Override
-			public boolean apply(ValueTypeDescription o) {
-				return "plain".equals(o.getId());
-			}
-		}));
+		// The 'plain' type must be first
+		assertEquals("plain", descriptions.get(0).getId());
+		assertFalse(descriptions.get(0).isParameterized());
 	}
 
 	@Test(expected = ValueTypeNotFoundException.class)
