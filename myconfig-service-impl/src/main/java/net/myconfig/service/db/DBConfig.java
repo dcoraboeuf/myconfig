ebbaaf5e3bf4acc99ccf2d79fdc577968fc46b63
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-10 19:58:25 +0100

    #117 'ordernb' column in the database

--- myconfig-service-impl/src/main/java/net/myconfig/service/db/DBConfig.java
+++ myconfig-service-impl/src/main/java/net/myconfig/service/db/DBConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 11;
+	public static final int VERSION = 12;
 	
 	@Autowired
 	private DataSource dataSource;
