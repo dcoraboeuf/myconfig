e5db9dcc2825321944c95de074c12602cd333493
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-28 21:07:50 +0100

    #130 Acceptance tests

--- myconfig-core/src/main/java/net/myconfig/core/model/ConfigurationDescription.java
+++ myconfig-core/src/main/java/net/myconfig/core/model/ConfigurationDescription.java
@@ -2,6 +2,9 @@ package net.myconfig.core.model;
 
 import java.util.List;
 
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import lombok.Data;
 
 import com.google.common.collect.ImmutableList;
@@ -12,7 +15,10 @@ public class ConfigurationDescription {
 	private final List<Environment> environments;
 	private final List<Key> keys;
 
-	public ConfigurationDescription(List<Environment> environments, List<Key> keys) {
+	@JsonCreator
+	public ConfigurationDescription(
+			@JsonProperty("environments") List<Environment> environments,
+			@JsonProperty("keys") List<Key> keys) {
 		this.environments = ImmutableList.copyOf(environments);
 		this.keys = ImmutableList.copyOf(keys);
 	}
