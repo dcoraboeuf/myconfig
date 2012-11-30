806bda3950cf4f77ad05e078be459b8beeb54449
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-11-30 21:05:56 +0100

    Make sure the security settings are disabled after a test has run

--- myconfig-service-impl/src/test/java/net/myconfig/service/impl/AbstractSecurityTest.java
+++ myconfig-service-impl/src/test/java/net/myconfig/service/impl/AbstractSecurityTest.java
@@ -20,6 +20,7 @@ import net.myconfig.service.message.TestPost;
 import net.myconfig.service.security.UserAuthentication;
 import net.myconfig.test.AbstractIntegrationTest;
 
+import org.junit.After;
 import org.junit.Before;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
@@ -99,6 +100,14 @@ public abstract class AbstractSecurityTest extends AbstractIntegrationTest {
 		for (String name : names) {
 			cacheManager.getCache(name).clear();
 		}
+		// No context
+		anonymous();
+	}
+	
+	@After
+	public void restoreContext() throws SQLException {
+		asAdmin();
+		securityService.setSecurityMode("none");
 		// No context
 		anonymous();
 	}
